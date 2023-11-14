9bc459f28d24d9b6f1e0773852b1d668be89ad5c
zjs <2455831260@qq.com>
2023-11-14 15:41:58 +0800

    新增classroom相关的一些方法

--- src/main/java/com/example/controller/ClassroomController.java
+++ src/main/java/com/example/controller/ClassroomController.java
@@ -4,11 +4,11 @@ import com.example.pojo.Classroom;
 import com.example.pojo.Result;
 import com.example.service.ClassroomService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @Slf4j
@@ -22,10 +22,35 @@ public class ClassroomController {
     @PostMapping("/add")
     public Result addClassroom(@RequestBody Classroom classroom) {
 
+        log.info("添加教室：{}", classroom.getClassroom());
+        int bool = classroomService.addClassroom(classroom);
+        if (bool == 0) {
+            return Result.error("已存在该教室");
+        }
         return Result.success();
     }
 
     //修改教室信息
+    @PostMapping("/update")
+    public Result updateClassroom(@RequestBody Classroom classroom) {
+
+        log.info("修改教室信息：{}", classroom.getClassroom());
+        classroomService.updateClassroom(classroom);
+        return Result.success();
+    }
 
     //查询教室
+    @GetMapping("/find")
+    public Result findClassroom() {
+        log.info("查询教室信息");
+        ArrayList<Classroom> classrooms = classroomService.SelectAllClassroom();
+        return Result.success(classrooms);
+    }
+
+    @DeleteMapping("/delete")
+    public Result deleteClassroom(@RequestBody Classroom classroom) {
+        log.info("查询指定教室");
+        classroomService.deleteClassroom(classroom);
+        return Result.success();
+    }
 }
--- src/main/java/com/example/service/ClassroomService.java
+++ src/main/java/com/example/service/ClassroomService.java
@@ -1,4 +1,17 @@
 package com.example.service;
 
+import com.example.pojo.Classroom;
+
+import java.util.ArrayList;
+
 public interface ClassroomService {
+
+
+    int addClassroom(Classroom classroom);
+
+    void updateClassroom(Classroom classroom);
+
+    ArrayList<Classroom> SelectAllClassroom();
+
+    void deleteClassroom(Classroom classroom);
 }
