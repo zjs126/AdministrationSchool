43c3172168ab101cdebd97e173408aecb2ddde38
wyt <126765604+Wytq15666688@users.noreply.github.com>
2023-12-06 08:13:31 +0800

    Delete AdministrationSchool-frontEnd directory

--- AdministrationSchool-frontEnd/src/api/student/course.js
+++
@@ -1,6 +0,0 @@
-import * as ajax from "../../common/ajax";
-
-export const list = () => ajax.pureGet("/student/searchScore");
-
-export const deleteItem = studentCourseId =>
-  ajax.pureDelete("/student/course/" + studentCourseId);
--- AdministrationSchool-frontEnd/src/api/student/info.js
+++
@@ -1,5 +0,0 @@
-import * as ajax from "../../common/ajax";
-
-export const get = () => ajax.pureGet("/student");
-
-export const update = entity => ajax.put("/student/update", entity);
