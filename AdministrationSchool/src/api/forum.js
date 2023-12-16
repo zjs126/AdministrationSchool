42ab7f0ccd2b78c2aed6e1b650ed5d6d29ac14a4
zjs <126223950+zjs126@users.noreply.github.com>
2023-12-16 12:26:50 +0800

    Delete AdministrationSchool directory

--- AdministrationSchool/src/api/forum.js
+++
@@ -1,9 +0,0 @@
-import { post, pureGet } from "../common/ajax";
-
-export const forum = (data) =>
-    post('/forum/postcomment', data)
-
-
-export const forumList = () =>
-    pureGet('/forum/findAllComment')
-
